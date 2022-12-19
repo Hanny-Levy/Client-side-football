8be7cfe22e53e4a1256072ee0aa8c8afdf93c365
Hanny <hannylevy63@gmail.com>
2022-12-20 00:49:40 +0200

    20.12-ZOOM

--- src/LiveResultPage1.js
+++ src/LiveResultPage1.js
@@ -1,35 +1,71 @@
 import React, {useEffect,useState} from 'react';
 import axios from "axios";
 
-const LiveResultPage1 = (props) => {
-    const [teamsLiveGames,setTeamsLiveGames]=useState([]);
+const LiveResultPage1 = () => {
+    const [liveGames,setLiveGames]=useState([]);
+    const [teamName,setTeamName] = useState("");
 
 
     useEffect(()=>{
-        axios.get("http://localhost:8989/getLiveResult").then((res)=>{
-            setTeamsLiveGames(res.data)
-            }
-        )
+        axios.get("http://localhost:8989/get-all-live-games").then((res)=>{
+            setLiveGames(res.data)
+            });
+
     } , [ ]);
 
 
 
+    const checkWinner = (goalsTeam1,goalsTeam2) => {
+        let color;
+        if (goalsTeam1===goalsTeam2){
+            color="yellow";
+        }else if (goalsTeam1>goalsTeam2){
+            color="red";
+        }else {
+            color="green"
+        }
+        return color ;
+    }
 
+    const getTeamName = (id) =>{
+        axios.get("http://localhost:8989/team-name-by-id", {
+            params: {
+                id : id
+            }
+        })
+            .then((res)=>{
+              setTeamName(res.data);
+        });
+    }
     return (
         <div >
 
-            <table border={1}>
+            <table border={1} color={"white"}>
+                <tr>
+                    <th>
+                       Name
+                    </th>
+                    <th>
+                        Name
+                    </th>
+                </tr>
                 {
-                    teamsLiveGames.map((game)=>{
+                    liveGames.map((game)=>{
+                        const team1Goals = game.team1GoalsFor-game.team1Against ;
+                        const team2Goals = game.team2GoalsFor-game.team2Against ;
+                        const teamName1=getTeamName(game.team1);
+                        const teamName2=getTeamName(game.team2);
+
                         return(
 
                                 <tr>
-                                    <td style={{color : game.team1.goalsFor > game.team2.goalsFor ? "green" : "red"}} >
+                                    <td style={{color : checkWinner(team1Goals,team2Goals)}} >
+                                        {teamName1}
 
-                                        {game.team1.name}
                                     </td>
-                                    <td>
-                                        {game.team2.name}
+                                    <td style={{color : checkWinner(team2Goals,team1Goals)}}>
+                                        {teamName2}
+
                                     </td>
                                 </tr>
 
